6280eaaedf802670a8ee60ab3599e291ec7d1528
Tacobatiz <149741394+Tacobatiz@users.noreply.github.com>
2025-05-19 20:15:26 -0400

    Update Main.cpp
    Added the third Cube

--- Rasterizer/Main.cpp
+++ Rasterizer/Main.cpp
@@ -19,9 +19,18 @@ int main() {
     };
 
     Model cube(face_colors);
+
+    Model scaledCube = cube;
+    for (auto& vertex : scaledCube.vertices) {
+        vertex.x *= 1.5f;
+        vertex.y *= 1.5f;
+        vertex.z *= 1.5f;
+    }
+
     std::vector<Instance> scene = {
-        {&cube, {-1.5f, -1.0f, 7.0f},-PI / 6, PI / 4, 0},
-        {&cube, { 1.25f,  1.0f, 7.5f},-PI / 6, PI / 4 + PI, 0}
+        {&cube, {-1.5f, -1.0f, 7.0f}, -PI / 6, PI / 4, 0},
+        {&cube, {1.25f, 1.0f, 7.5f}, -PI / 6, PI / 4 + PI, 0},
+        {&scaledCube, {2.0f, -2.0f, 6.0f}, PI / 8, -PI / 5, 0}
     };
 
     rz.draw(scene);
